6b1b4ce347aed84c840b85711b815199e5fe2cca
ITDranik <itdranik@gmail.com>
2020-03-21 17:12:19 +0200

    Squashed commit of the following:
    commit ac04b01d75a6987ae46c812f938609db2c294a84
    Author: ITDranik <itdranik@gmail.com>
    Date:   Sat Mar 21 17:10:45 2020 +0200
    
        cs-solvers-#8 lru-cache
    
    commit bf08de661d65175cbfc2e064e680d291f0817c6d
    Author: ITDranik <itdranik@gmail.com>
    Date:   Sat Mar 21 17:07:07 2020 +0200
    
        cs-solvers-#8 sub-optimal lru-cache
    
    commit e357193167999ac62d044ae4cd50e34dba67f461
    Author: ITDranik <itdranik@gmail.com>
    Date:   Sat Mar 21 16:06:47 2020 +0200
    
        cs-solvers-#8 simple cache

---
+++ cs-solvers/ITDranik/CodingInterview/Solvers/Caching/LruCache.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace ITDranik.CodingInterview.Solvers.Caching {
+    internal class LruEntry<TKey, TValue> {
+
+        public LruEntry(TKey key, TValue value) {
+            _key = key;
+            _value = value;
+        }
+
+        public TKey Key { get { return _key; } }
+        public TValue Value { get { return _value; } }
+
+        private readonly TKey _key;
+        private readonly TValue _value;
+    }
+
+    public class LruCache<TKey, TValue> {
+        public LruCache(int capacity) {
+            if (capacity <= 0) {
+                var exMessage = $"must be a positive value";
+                throw new System.ArgumentOutOfRangeException(nameof(capacity), capacity, exMessage);
+            }
+
+            _capacity = capacity;
+            _cache = new Dictionary<TKey, LinkedListNode<LruEntry<TKey, TValue>>>();
+            _nodes = new LinkedList<LruEntry<TKey, TValue>>();
+        }
+
+        public void Add(TKey key, TValue value) {
+            if (_cache.Count < _capacity) {
+                AddNewEntry(key, value);
+            } else if (_cache.ContainsKey(key)) {
+                UpdateEntry(key, value);
+            } else {
+                ReplaceOldEntry(key, value);
+            }
+        }
+
+        private void AddNewEntry(TKey key, TValue value) {
+            var entry = new LruEntry<TKey, TValue>(key, value);
+            _cache[key] = _nodes.AddFirst(entry);
+        }
+
+        private void UpdateEntry(TKey key, TValue value) {
+            Remove(key);
+            AddNewEntry(key, value);
+        }
+
+        private void ReplaceOldEntry(TKey key, TValue value) {
+            var minKey = _nodes.Last.Value.Key;
+            Remove(minKey);
+            AddNewEntry(key, value);
+        }
+
+        public bool TryGet(TKey key, [MaybeNullWhen(false)] out TValue value) {
+            if (!_cache.ContainsKey(key)) {
+                value = default;
+                return false;
+            }
+
+            value = GetEntry(key);
+            return true;
+        }
+
+        private TValue GetEntry(TKey key) {
+            var node = _cache[key];
+            Remove(key);
+            var entry = node.Value;
+            AddNewEntry(entry.Key, entry.Value);
+            return entry.Value;
+        }
+
+        public bool Remove(TKey key) {
+            _nodes.Remove(_cache[key]);
+            return _cache.Remove(key);
+        }
+
+        public void Clear() {
+            _nodes.Clear();
+            _cache.Clear();
+        }
+
+        private readonly int _capacity;
+        private readonly Dictionary<TKey, LinkedListNode<LruEntry<TKey, TValue>>> _cache;
+        private readonly LinkedList<LruEntry<TKey, TValue>> _nodes;
+    }
+}
---
+++ cs-solvers/ITDranik/CodingInterview/SolversTests/Caching/LruCacheTests.cs
@@ -0,0 +1,106 @@
+using FluentAssertions;
+using ITDranik.CodingInterview.Solvers.Caching;
+using Xunit;
+
+namespace ITDranik.CodingInterview.SolversTests.Caching {
+    public class LruCacheTests {
+        [Fact]
+        public void GetNonexistentItemTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.TryGet("item", out var _).Should().BeFalse();
+        }
+
+        [Fact]
+        public void AddToCacheTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.Add("item", 1);
+
+            cache.TryGet("item", out var actual).Should().BeTrue();
+            actual.Should().Be(1);
+        }
+
+        [Fact]
+        public void OverwriteTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.Add("item", 1);
+            cache.Add("item", 2);
+
+            cache.TryGet("item", out var actual).Should().BeTrue();
+            actual.Should().Be(2);
+        }
+
+        [Fact]
+        public void RemoveItemTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.Add("item", 1);
+            cache.Remove("item").Should().BeTrue();
+
+            cache.TryGet("item", out var _).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ClearCacheTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.Add("item1", 1);
+            cache.Add("item2", 2);
+
+            cache.Clear();
+
+            cache.TryGet("item1", out var _1).Should().BeFalse();
+            cache.TryGet("item2", out var _2).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ExceedCapacityTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.Add("item1", 1);
+            cache.Add("item2", 2);
+            cache.Add("item3", 3);
+            cache.Add("item4", 4);
+
+            cache.TryGet("item1", out var item1).Should().BeFalse();
+            cache.TryGet("item2", out var item2).Should().BeTrue();
+            item2.Should().Be(2);
+            cache.TryGet("item3", out var item3).Should().BeTrue();
+            item3.Should().Be(3);
+            cache.TryGet("item4", out var item4).Should().BeTrue();
+            item4.Should().Be(4);
+        }
+
+        [Fact]
+        public void ExceedCapacityWithChangedPriorityAfterAccessTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.Add("item1", 1);
+            cache.Add("item2", 2);
+            cache.Add("item3", 3);
+            cache.TryGet("item1", out var _);
+            cache.Add("item4", 4);
+
+            cache.TryGet("item1", out var item1).Should().BeTrue();
+            item1.Should().Be(1);
+            cache.TryGet("item2", out var item2).Should().BeFalse();
+            cache.TryGet("item3", out var item3).Should().BeTrue();
+            item3.Should().Be(3);
+            cache.TryGet("item4", out var item4).Should().BeTrue();
+            item4.Should().Be(4);
+        }
+
+        [Fact]
+        public void ExceedCapacityWithChangedPriorityAfterUpdateTest() {
+            var cache = new LruCache<string, int>(3);
+            cache.Add("item1", 1);
+            cache.Add("item2", 2);
+            cache.Add("item3", 3);
+            cache.Add("item1", 1);
+            cache.Add("item4", 4);
+
+            cache.TryGet("item1", out var item1).Should().BeTrue();
+            item1.Should().Be(1);
+            cache.TryGet("item2", out var item2).Should().BeFalse();
+            cache.TryGet("item3", out var item3).Should().BeTrue();
+            item3.Should().Be(3);
+            cache.TryGet("item4", out var item4).Should().BeTrue();
+            item4.Should().Be(4);
+        }
+    }
+}
